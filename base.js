bc75d39c72277a2f07d853c3b8a24d5d75b61e1d
SurplusFate <63409462+SurplusFate@users.noreply.github.com>
2022-10-12 00:40:02 +0000

    Deploying to master from @ SurplusFate/SurplusFate.github.io@931d1bc2fde96e0d2efe35121f2e704b1b1ec467 🚀

--- base.js
+++ base.js
@@ -1,1 +1,14 @@
-console.log("5");
+// 随机一言
+var hitokoto = document.getElementsByClassName("footer")[0];
+console.log(hitokoto);
+if (hitokoto != undefined) {
+  var xhr = new XMLHttpRequest();
+  xhr.open("get", "https://v1.hitokoto.cn");
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4) {
+      var data = JSON.parse(xhr.responseText);
+      hitokoto.innerText = data.hitokoto + "  --" + data.from;
+    }
+  };
+  xhr.send();
+}
